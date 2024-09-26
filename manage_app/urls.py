cf2ce02c593339663be53f6a37c76016b4fa462d
katsuharu.kaita <kaitakatsuharu@minadenmasaruonnoMacBook-Pro.local>
2024-09-27 04:16:19 +0900

    add bootstrap5

--- manage_app/urls.py
+++ manage_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .import views
-app_name='manage_app/'
+app_name='manage_app'
 
 urlpatterns=[
     path('',views.index,name='index'),
--- manage_app/views.py
+++ manage_app/views.py
@@ -12,7 +12,7 @@ def add(request):
     form = DayCreateForm(request.POST or None)
     if request.method=='POST' and form.is_valid():
         form.save
-        return redirect('diary:index')
+        return redirect('manage_app:index')
     context={
         'form':form
     }
