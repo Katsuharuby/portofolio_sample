38670ee35927158235342d203bd15b3898a762f3
katsuharu.kaita <kaitakatsuharu@minadenmasaruonnoMacBook-Pro.local>
2024-09-27 03:59:52 +0900

    change settings py add static/manage_app/css  and style.css

--- manage_app/urls.py
+++ manage_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .import views
-app_name='manage_app'
+app_name='manage_app/'
+
 urlpatterns=[
     path('',views.index,name='index'),
     path('add/',views.add,name='add'),
--- manage_app/views.py
+++ manage_app/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import DayCreateForm
+from .models import Day
 
 def index(request):
-    return render(request,'manage_app/day_list.html')
+    context={
+        'day_list':Day.objects.all()
+    }
+    return render(request,'manage_app/day_list.html',context)
 
 def add(request):
+    form = DayCreateForm(request.POST or None)
+    if request.method=='POST' and form.is_valid():
+        form.save
+        return redirect('diary:index')
     context={
-        'form':DayCreateForm()
+        'form':form
     }
     return render(request,'manage_app/day_form.html',context)
