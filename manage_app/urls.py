a9a820acefce215bae28d73447c4d0c341fad9d4
katsuharu.kaita <kaitakatsuharu@minadenmasaruonnoMacBook-Pro.local>
2024-10-01 03:41:49 +0900

    signupページの作成

--- manage_app/urls.py
+++ manage_app/urls.py
@@ -12,4 +12,5 @@ urlpatterns=[
     path('detail/<int:pk>',views.DetailView.as_view(),name='detail'),
     path('login/', auth_views.LoginView.as_view(template_name='manage_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
